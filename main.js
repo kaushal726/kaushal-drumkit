95686b9798b416fdc88841fbf31da63c1a3480b4
Kaushal <Kausraj11@gmail.com>
2023-01-24 22:28:14 +0530

    Changes Updated

--- main.js
+++ main.js
@@ -7,39 +7,67 @@ let smalltom = document.querySelector(".f");
 let snare = document.querySelector(".g");
 let splash = document.querySelector(".h");
 
-bass.addEventListener('click',()=>console.log(1212));
-bassDrum = (e) => {
-    const audioBashDrum = new Audio("./asseset/audio/bash.mp3");
-    if (e.key == 'z') {
-        audioBashDrum.play();
-        bass.click();
+document.addEventListener("keypress", (e) => {
+    switch (e.key) {
+        case "a":
+            bassDrum();
+            break;
+        case "s":
+            hiHit();
+            break;
+        case "d":
+            mediumTom();
+            break;
+        case "f":
+            mediumTom2();
+            break;
+        case "g":
+            ride1();
+            break;
+        case "h":
+            smallTom();
+            break;
+        case "j":
+            snare1();
+            break;
+        case "k":
+            splash1();
+            break;
+
+        default:
+            break;
     }
+})
+
+bassDrum = () => {
+    const audioBashDrum = new Audio("./asseset/audio/bash.mp3");
+    return (audioBashDrum.play());
 }
 function hiHit() {
     const audiohiHit = new Audio("./asseset/audio/hithat.mp3");
-    audiohiHit.play();
+    return (audiohiHit.play());
 }
 function mediumTom() {
     const audiomediumTom = new Audio("./asseset/audio/mediumtom.mp3");
-    audiomediumTom.play();
+    return (audiomediumTom.play());
 }
 function mediumTom2() {
     const audiomediumTom2 = new Audio("./asseset/audio/tom.mp3");
-    audiomediumTom2.play();
+    return (audiomediumTom2.play());
 }
 function ride1() {
     const audioride = new Audio("./asseset/audio/ridecrash.mp3");
-    audioride.play();
+    return (audioride.play());
 }
 function smallTom() {
     const audioBashDrum = new Audio("./asseset/audio/smalltom.mp3");
-    audioBashDrum.play();
+    return (audioBashDrum.play());
 }
 function snare1() {
     const audioBashDrum = new Audio("./asseset/audio/snare.mp3");
-    audioBashDrum.play();
+    return (audioBashDrum.play());
 }
 function splash1() {
     const audioBashDrum = new Audio("./asseset/audio/splash.mp3");
-    audioBashDrum.play();
+    return (audioBashDrum.play());
 }
